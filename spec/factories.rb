ebc79f8597e3e2f43ad9a0529fdc804c80631ac1
Alex Konrad <alexkonrad08@gmail.com>
2013-07-11 22:10:47 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name 									"Alex Konrad"
+		email 								"akonrad@wisc.edu"
+		password 							"akonrad1"
+		password_confirmation "akonrad1"
+	end
+end
