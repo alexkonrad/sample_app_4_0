5ea32d3c0e5507efaccd9946d07cfa80fd0e0cb2
Alex Konrad <alexkonrad08@gmail.com>
2013-07-15 22:51:56 -0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,8 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-		#user = User.find_by_email(params[:email].downcase)
-		#if user && user.authenticate(params[:password])
 			sign_in user
-			redirect_to user	
+			redirect_back_or user	
 		else
 			flash.now[:error] = 'Invalid email/password combination'
 			render 'new'
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name 									"Alex Konrad"
-		email 								"akonrad@wisc.edu"
-		password 							"akonrad1"
-		password_confirmation "akonrad1"
+		sequence(:name)		{ |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" }
+		password 							"amkamk"
+		password_confirmation "amkamk"
+		
+		factory :admin do
+			admin true
+		end
 	end
 end
